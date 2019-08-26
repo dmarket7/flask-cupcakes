de15c78319fb8909955f656a2272ec4c22d21858
jyahn <jyxahn94@gmail.com>
2019-08-26 14:09:27 -0700

    Implemented create, update,and delete routes

--- app.py
+++ app.py
@@ -30,3 +30,69 @@ def get_cupcakes():
 
     return jsonify(cupcakes=serialized_cupcakes)
 
+
+@app.route('/api/cupcakes/<int:cupcake_id>')
+def get_cupcake(cupcake_id):
+    """ Get one cupcake"""
+
+    cupcake_from_db = Cupcake.query.get_or_404(cupcake_id)
+    serialized_cupcake = serialize_cupcake(cupcake_from_db)
+
+    return jsonify(cupcake=serialized_cupcake)
+
+
+@app.route('/api/cupcakes', methods=["POST"])
+def create_new_cupcake():
+    """ Create new cupcake"""
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized_cupcake = serialize_cupcake(new_cupcake)
+
+    return (jsonify(cupcake=serialized_cupcake), 201)
+
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=["PATCH"])
+def update_cupcake(cupcake_id):
+    """ Updates existing cupcake"""
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+    
+    cupcake_from_db = Cupcake.query.get_or_404(cupcake_id)
+    cupcake_from_db.flavor = flavor
+    cupcake_from_db.size = size
+    cupcake_from_db.rating = rating
+    cupcake_from_db.image = image
+
+    db.session.add(cupcake_from_db)
+    db.session.commit()
+
+    serialized_cupcake = serialize_cupcake(cupcake_from_db)
+
+    return (jsonify(cupcake=serialized_cupcake))
+
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=["DELETE"])
+def delete_cupcake(cupcake_id):
+    """Deletes an existing cupcake"""
+
+    cupcake_from_db = Cupcake.query.get_or_404(cupcake_id)
+
+    db.session.delete(cupcake_from_db)
+    db.session.commit()
+
+
+    return (jsonify({"message": "Cupcake deleted"}))
