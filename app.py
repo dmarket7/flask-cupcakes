69fd774490256b3be31bec571eaf4b2751584a49
Dario Mercado <dariomercado@RICHARDs-MBP.localdomain>
2019-08-26 12:05:39 -0700

    got cupcakes list route to work

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
 """Flask app for Cupcakes"""
+from flask import Flask, redirect, render_template, request, flash, jsonify
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "any secret name"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql:///cupcakes'
+app.config["SQLALCHEMY_TRACK_MODIFCATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+connect_db(app)
+
+def serialize_cupcake(cupcake):
+    """ Serialize a cupcake """
+
+    return {
+        "id": cupcake.id,
+        "flavor": cupcake.flavor,
+        "size": cupcake.size,
+        "rating": cupcake.rating,
+        "image": cupcake.image
+    }
+
+@app.route('/api/cupcakes')
+def get_cupcakes():
+    """ Get list of all cupcakes """
+
+    cupcakes_from_db = Cupcake.query.all()
+    serialized_cupcakes = [serialize_cupcake(cupcake) for cupcake in cupcakes_from_db]
+
+    return jsonify(cupcakes=serialized_cupcakes)
+
--- models.py
+++ models.py
@@ -10,3 +10,19 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
+class Cupcake(db.Model):
+    """cupcake"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    flavor = db.Column(db.Text,
+                     nullable=False)
+    size = db.Column(db.Text,
+                     nullable=False)
+    rating = db.Column(db.Float,
+                     nullable=False)
+    image = db.Column(db.Text,
+                    default="https://tinyurl.com/demo-cupcake")
