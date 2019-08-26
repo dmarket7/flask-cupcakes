4c4b32e32f8db34bee10c220e388ec38f4b7dd84
Dario Mercado <dariomercado@RICHARDs-MBP.localdomain>
2019-08-26 16:48:13 -0700

    make cupcake links

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -1,49 +1,53 @@
 $(async function () {
-    const $cupcakesContainer = $('#cupcakes-container');
-    const $cupcakeForm = $('#cupcake-form');
-    const $cupcakeFlavor = $('#cupcake-flavor');
-    const $cupcakeSize = $('#cupcake-size');
-    const $cupcakeRating = $('#cupcake-rating');
-    const $cupcakeImage = $('#cupcake-image');
-    const $cupcakesList = $('#cupcakes-list')
+  const $cupcakesContainer = $('#cupcakes-container');
+  const $cupcakeForm = $('#cupcake-form');
+  const $cupcakeFlavor = $('#cupcake-flavor');
+  const $cupcakeSize = $('#cupcake-size');
+  const $cupcakeRating = $('#cupcake-rating');
+  const $cupcakeImage = $('#cupcake-image');
+  const $cupcakesList = $('#cupcakes-list')
 
 
-    $cupcakeForm.on("submit", async function (e) {
-        e.preventDefault()
+  $cupcakeForm.on("submit", async function (e) {
+    e.preventDefault()
 
-        let formCupcakeFlavor = $cupcakeFlavor.val()
-        let formCupcakeSize = $cupcakeSize.val()
-        let formCupcakeRating = $cupcakeRating.val()
-        let formCupcakeImage = $cupcakeImage.val()
+    let formCupcakeFlavor = $cupcakeFlavor.val()
+    let formCupcakeSize = $cupcakeSize.val()
+    let formCupcakeRating = $cupcakeRating.val()
+    let formCupcakeImage = $cupcakeImage.val()
 
-        let newCupcake = {
-            "flavor": formCupcakeFlavor,
-            "size": formCupcakeSize,
-            "rating": formCupcakeRating,
-            "image": formCupcakeImage
-        }
-        let newlyCreatedCupcake = await axios.post("/api/cupcakes", newCupcake);
-        let newCupcakeElement= $("<li>")
-        console.log(newlyCreatedCupcake)
-        newCupcakeElement.text(newlyCreatedCupcake.data.cupcake.flavor);
-        $cupcakesList.append(newCupcakeElement);
+    let newCupcake = {
+      "flavor": formCupcakeFlavor,
+      "size": formCupcakeSize,
+      "rating": formCupcakeRating,
+      "image": formCupcakeImage
+    }
+    let newlyCreatedCupcake = await axios.post("/api/cupcakes", newCupcake);
+    let newlyCreatedFlavor = newlyCreatedCupcake.data.cupcake.flavor;
+    let newlyCreatedImage = newlyCreatedCupcake.data.cupcake.image;
+    let $newCupcakeElement = $("<li>")
+    let $newCupcakeAtag = $(`<a href="${newlyCreatedImage}">${newlyCreatedFlavor}</a>`)
+    $newCupcakeElement.append($newCupcakeAtag)
+    $cupcakesList.append($newCupcakeElement);
 
-    
-    })
 
-    async function showCupcakes() {
+  })
 
-        let cupcakesFromDB = await axios.get("/api/cupcakes");
-        console.log(cupcakesFromDB)
-        for (var i = 0; i < cupcakesFromDB.data.cupcakes.length; i++) {
-            let newCupcake = $("<li>")
-            newCupcake.text(cupcakesFromDB.data.cupcakes[i].flavor);
-            $cupcakesList.append(newCupcake);
-        }
+  async function showCupcakes() {
 
+    let cupcakesFromDB = await axios.get("/api/cupcakes");
+    for (var i = 0; i < cupcakesFromDB.data.cupcakes.length; i++) {
+      let newlyCreatedFlavor = cupcakesFromDB.data.cupcakes[i].flavor;
+      let newlyCreatedImage = cupcakesFromDB.data.cupcakes[i].image;
+      let $newCupcake = $("<li>")
+      let $newCupcakeAtag = $(`<a href="${newlyCreatedImage}">${newlyCreatedFlavor}</a>`)
+      $newCupcake.append($newCupcakeAtag)
+      $cupcakesList.append($newCupcake);
     }
 
-    showCupcakes();
+  }
+
+  showCupcakes();
 
 
 
